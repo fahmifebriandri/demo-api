6d75147156e9d7f913c5375c9b7fae81ecfe3bb3
fahmifebriandri <fahmifebriandri@gmail.com>
2023-05-27 19:07:27 +0700

    demo api origin

---
+++ src/main/java/com/domain/demoapi/DemoApiApplication.java
@@ -0,0 +1,18 @@
+package com.domain.demoapi;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+}
